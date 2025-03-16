517e0f5a8fdd84cb920ce06c679f455110fab28c
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-03-15 22:05:17 -0500

    Updated header styling, logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,35 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Header() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.leftSide}>
-          <Link to="/" className={styles.logo}>
-            Logo Here
+          <Link to="/" className={styles.logoContainer}>
+            <img src={logo} alt="Logo" id={styles.logo} />
           </Link>
           <Link to="category1" className={styles.headerLinks}>
-            cat1
+            Cameras
           </Link>
           <Link to="category2" className={styles.headerLinks}>
-            cat2
+            Tripods
           </Link>
           <Link to="category3" className={styles.headerLinks}>
-            cat3
+            Accessories
           </Link>
         </div>
         <div className={styles.rightSide}>
           <span className="material-icons">search</span>
+          <span>Search</span>
           <span className="material-icons">account_circle</span>
+          <span>Account</span>
           <span className="material-icons">shopping_cart</span>
+          <span>Cart:</span>
         </div>
       </div>
+      <div></div>
     </div>
   );
 }
