46ac2b0716293ee1d2a09bbd86a582b8b2a31df4
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-03-17 20:50:31 -0500

    Added item name and price fetch, basic formatting

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,12 @@
 import styles from "./ProductCard.module.css";
 import { useState, useEffect } from "react";
 
-function useImageURL({ itemID }) {
-  const [imageURL, setImageURL] = useState(null);
+function useProductData({ itemID }) {
+  const [productData, setProductData] = useState({
+    image: null,
+    name: "",
+    price: "",
+  });
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -15,35 +19,49 @@ function useImageURL({ itemID }) {
         }
         return response.json();
       })
-      .then((response) => setImageURL(response.image))
+      .then((response) =>
+        setProductData({
+          image: response.image,
+          name: response.title,
+          price: response.price,
+        })
+      )
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
   }, [itemID]);
 
-  return { imageURL, error, loading };
+  return { productData, error, loading };
 }
 
-function Image({ itemID }) {
-  //set product state i think
+// function Image({ itemID }) {
+//   //set product state i think
 
-  const { imageURL, error, loading } = useImageURL({ itemID });
+//   const { imageURL, error, loading } = useImageURL({ itemID });
+
+//   return (
+//     <>
+//       {/* <h1>An image</h1> */}
+//       <img src={imageURL} alt={"placeholder text"} />
+//     </>
+//   );
+// }
+
+function ProductCard({ itemID }) {
+  const { productData, error, loading } = useProductData({ itemID });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
   return (
-    <>
-      {/* <h1>An image</h1> */}
-      <img src={imageURL} alt={"placeholder text"} />
-    </>
-  );
-}
-
-function ProductCard({ itemID }) {
-  return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image itemID={itemID}></Image>
+        <img src={productData.image} alt={productData.name} />
+      </div>
+      <div className={styles.infoContainer}>
+        <h3 className={styles.itemName}>{productData.name}</h3>
+        <span className={styles.itemPrice}>
+          ${productData.price.toFixed(2)}
+        </span>
       </div>
     </div>
   );
