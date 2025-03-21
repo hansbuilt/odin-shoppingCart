b090640ed8ae02653e3042428cdb03476d89033c
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-03-20 21:05:59 -0500

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-
 import Homepage from "./pages/Homepage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Homepage></Homepage>
