c00a47fb7f1fc22d6b7eb96902a94fc003d1e7fa
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-03-25 20:45:12 -0500

    Updated homepage content

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,18 @@
 import styles from "./Homepage.module.css";
+import home_banner from "../assets/homeBanner.webp";
 
 function Homepage() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <span>homepage content here</span>
+        <div className={styles.homeBannerContainer}>
+          {/* <img src={home_banner} alt="Banner" /> */}
+
+          <span className={styles.homeBannerText}>
+            Welcome to a BETTER place to browse, buy, and question your
+            financial choices!
+          </span>
+        </div>
       </div>
     </div>
   );
