0993404f3dd57332aacd1017675110ca85032e71
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2025-03-26 22:22:21 -0500

    Added overlay over page during cart flyout to prevent actions outside it, click to close

--- src/components/CartFlyout.jsx
+++ src/components/CartFlyout.jsx
@@ -23,65 +23,74 @@ function CartFlyout() {
   );
 
   return (
-    <div
-      className={`${styles.container} ${isCartVisible ? styles.visible : ""}`}
-    >
-      <div>
-        <button onClick={hideCart}>close cart</button>
-      </div>
-      <div className={styles.cartItems}>
-        <table>
-          <tbody>
-            <tr>
-              <th>#</th>
-              <th>Desc</th>
-              <th></th>
-              <th>Qty</th>
-              <th></th>
-              <th>Price</th>
-            </tr>
+    <>
+      <div
+        className={`${styles.cartOverlay} ${
+          isCartVisible ? styles.visible : ""
+        }`}
+        onClick={hideCart}
+      ></div>
 
-            {Object.entries(cart).map(([itemID, item]) => (
-              <CartItem
-                key={crypto.randomUUID()}
-                itemID={itemID}
-                name={cropString(item.name, 50)}
-                quantity={item.quantity}
-                price={item.price}
-                extendedPrice={item.extendedPrice}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <div
+        className={`${styles.container} ${isCartVisible ? styles.visible : ""}`}
+      >
+        <div>
+          <button onClick={hideCart}>close cart</button>
+        </div>
+        <div className={styles.cartItems}>
+          <table>
+            <tbody>
+              <tr>
+                <th>#</th>
+                <th>Desc</th>
+                <th></th>
+                <th>Qty</th>
+                <th></th>
+                <th>Price</th>
+              </tr>
 
-      <div className={styles.cartTotal}>
-        <table>
-          <tbody>
-            <tr>
-              <th>Subtotal</th>
-              <td>${orderTotal.toFixed(2)}</td>
-            </tr>
-            <tr>
-              <th>Shipping</th>
-              <td>$0.00</td>
-            </tr>
-            <tr>
-              <th>Tax</th>
-              <td>$0.00</td>
-            </tr>
-            <tr>
-              <th>Order Total</th>
-              <td>${orderTotal.toFixed(2)}</td>
-            </tr>
-          </tbody>
-        </table>
-        <button className={styles.buttonCheckout}>Checkout</button>
-        <button onClick={hideCart} className={styles.buttonKeepShopping}>
-          Keep Shopping
-        </button>
+              {Object.entries(cart).map(([itemID, item]) => (
+                <CartItem
+                  key={crypto.randomUUID()}
+                  itemID={itemID}
+                  name={cropString(item.name, 50)}
+                  quantity={item.quantity}
+                  price={item.price}
+                  extendedPrice={item.extendedPrice}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className={styles.cartTotal}>
+          <table>
+            <tbody>
+              <tr>
+                <th>Subtotal</th>
+                <td>${orderTotal.toFixed(2)}</td>
+              </tr>
+              <tr>
+                <th>Shipping</th>
+                <td>$0.00</td>
+              </tr>
+              <tr>
+                <th>Tax</th>
+                <td>$0.00</td>
+              </tr>
+              <tr>
+                <th>Order Total</th>
+                <td>${orderTotal.toFixed(2)}</td>
+              </tr>
+            </tbody>
+          </table>
+          <button className={styles.buttonCheckout}>Checkout</button>
+          <button onClick={hideCart} className={styles.buttonKeepShopping}>
+            Keep Shopping
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
